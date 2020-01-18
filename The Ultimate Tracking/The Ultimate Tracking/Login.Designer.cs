0bbb48d4e626b8cd88c43d9230189edf88d27d1c
ltphong2003 <queanh150874@gmail.com>
2020-01-18 22:26:04 +0700

    Add signup page

---
+++ The Ultimate Tracking/The Ultimate Tracking/Login.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace The_Ultimate_Tracking
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmailLogin = new System.Windows.Forms.TextBox();
+            this.txtPasswordLogin = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEmailSignup = new System.Windows.Forms.TextBox();
+            this.txtPasswordSignup = new System.Windows.Forms.TextBox();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(374, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Password";
+            // 
+            // txtEmailLogin
+            // 
+            this.txtEmailLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmailLogin.Location = new System.Drawing.Point(134, 15);
+            this.txtEmailLogin.Name = "txtEmailLogin";
+            this.txtEmailLogin.Size = new System.Drawing.Size(234, 36);
+            this.txtEmailLogin.TabIndex = 3;
+            // 
+            // txtPasswordLogin
+            // 
+            this.txtPasswordLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPasswordLogin.Location = new System.Drawing.Point(547, 16);
+            this.txtPasswordLogin.Name = "txtPasswordLogin";
+            this.txtPasswordLogin.Size = new System.Drawing.Size(234, 36);
+            this.txtPasswordLogin.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(796, 15);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(111, 40);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(466, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(381, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Don\'t have an account ?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(466, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 39);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(466, 389);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 39);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Password";
+            // 
+            // txtEmailSignup
+            // 
+            this.txtEmailSignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmailSignup.Location = new System.Drawing.Point(673, 311);
+            this.txtEmailSignup.Name = "txtEmailSignup";
+            this.txtEmailSignup.Size = new System.Drawing.Size(234, 36);
+            this.txtEmailSignup.TabIndex = 9;
+            // 
+            // txtPasswordSignup
+            // 
+            this.txtPasswordSignup.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPasswordSignup.Location = new System.Drawing.Point(673, 389);
+            this.txtPasswordSignup.Name = "txtPasswordSignup";
+            this.txtPasswordSignup.Size = new System.Drawing.Size(234, 36);
+            this.txtPasswordSignup.TabIndex = 10;
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.Location = new System.Drawing.Point(648, 460);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(111, 40);
+            this.btnSignup.TabIndex = 11;
+            this.btnSignup.Text = "Sign up";
+            this.btnSignup.UseVisualStyleBackColor = true;
+            this.btnSignup.Click += new System.EventHandler(this.btnSignup_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 666);
+            this.Controls.Add(this.btnSignup);
+            this.Controls.Add(this.txtPasswordSignup);
+            this.Controls.Add(this.txtEmailSignup);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPasswordLogin);
+            this.Controls.Add(this.txtEmailLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmailLogin;
+        private System.Windows.Forms.TextBox txtPasswordLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEmailSignup;
+        private System.Windows.Forms.TextBox txtPasswordSignup;
+        private System.Windows.Forms.Button btnSignup;
+    }
+}
+
---
+++ The Ultimate Tracking/The Ultimate Tracking/Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Database;
+
+namespace The_Ultimate_Tracking
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignup_Click(object sender, EventArgs e)
+        {
+            if ((txtEmailSignup.Text!="")&&(txtPasswordSignup.Text!=""))
+            {
+                UserAcc new_user = new UserAcc
+                {
+                    email = txtEmailSignup.Text,
+                    password = txtPasswordSignup.Text
+                };
+               User.InsertUser(new_user);
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
